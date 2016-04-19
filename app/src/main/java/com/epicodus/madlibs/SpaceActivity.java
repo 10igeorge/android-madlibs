ed505f68ff8af8ecfdcb945e8268119d30cabe95
Izzy George <10izzygeorge@gmail.com>
2016-04-19 11:44:26 -0700

    create view for space mad libs user input page

--- app/src/main/java/com/epicodus/madlibs/SpaceActivity.java
+++ app/src/main/java/com/epicodus/madlibs/SpaceActivity.java
@@ -1,13 +1,19 @@
 package com.epicodus.madlibs;
 
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class SpaceActivity extends AppCompatActivity {
+    private Button mSubmitSpaceButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_space);
+
+
     }
 }
