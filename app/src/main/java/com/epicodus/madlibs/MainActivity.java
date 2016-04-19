91e980e50fd55ba4fd4f59ea8048aaba780196db
Izzy George <10izzygeorge@gmail.com>
2016-04-19 15:49:12 -0700

    revamp home page

--- app/src/main/java/com/epicodus/madlibs/MainActivity.java
+++ app/src/main/java/com/epicodus/madlibs/MainActivity.java
@@ -1,10 +1,12 @@
 package com.epicodus.madlibs;
 
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     private Button mSpaceButton;
@@ -13,6 +15,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView mLogo = (TextView) findViewById(R.id.logoTextView);
+        Typeface mSigmarOne = Typeface.createFromAsset(getAssets(), "SigmarOne.ttf");
+        mLogo.setTypeface(mSigmarOne);
 
         mSpaceButton = (Button) findViewById(R.id.spaceButton);
         mSpaceButton.setOnClickListener(new View.OnClickListener() {
